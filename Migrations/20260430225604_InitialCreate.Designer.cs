3554b935372be5bd8a064e04e8b74c79ca896154
gastontecheradev <acorde.musica10@gmail.com>
2026-06-07 16:01:10 -0300

    Commit

--- Migrations/20260430225604_InitialCreate.Designer.cs
+++ Migrations/20260430225604_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using PasearPorPasear.Data;
 namespace PasearPorPasear.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260427192716_InitialCreate")]
+    [Migration("20260430225604_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
